d17d687955b00cc32e91eb29c63f6bb26eb0e14b
alvihelene <95623140+alvihelene@users.noreply.github.com>
2022-03-12 22:29:50 +0100

    Kampagneknap fungerer
    Den skal bare ned hvor den hører til på siden :-)

--- kampagne.js
+++ kampagne.js
@@ -1,10 +1,3 @@
-/* Kampagneknap */
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  /* Kampagneknap slut*/
+function toggleImage(){
+  document.querySelector('#image').classList.toggle('hidden');
+}
